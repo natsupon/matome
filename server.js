d38db765b9abd67f36aa833d7df248c4808afada
natsumiarai <amna.frais.o.07@gmail.com>
2021-08-01 14:23:07 +0900

    all sample

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-t express = require('express');
-const serveStatic = require("serve-static");
+const express = require('express');
+const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
